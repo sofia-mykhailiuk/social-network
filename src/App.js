ea78285856a4c08f57819ae96b571e09db7ab966
Sofia Mykhailiuk <sofijka.ukraine@gmail.com>
2021-08-10 23:04:42 +0300

    19. add routing

--- src/App.js
+++ src/App.js
@@ -3,18 +3,29 @@ import Header from "./Components/Header/Header";
 import Navbar from "./Components/Navbar/Navbar";
 import Profile from "./Components/Profile/Profile";
 import Dialogs from "./Components/Dialogs/Dialogs";
+import News from "./Components/News/News";
+import Music from "./Components/Music/Music";
+import Settings from "./Components/Settings/Settings";
+import {BrowserRouter, Route} from "react-router-dom";
 
 
 const App = () => {
     return (
-        <div className='app-wrapper'>
-            <Header />
-            <Navbar />
-            <div className='app-wrapper-content'>
-                <Dialogs />
-                {/*<Profile/>*/}
+        <BrowserRouter>
+            <div className='app-wrapper'>
+                <Header/>
+                <Navbar/>
+                <div className='app-wrapper-content'>
+                    <Route path='/profile' component={Profile}/>
+                    <Route path='/dialogs' component={Dialogs}/>
+                    <Route path='/news' component={News}/>
+                    <Route path='/music' component={Music}/>
+                    <Route path='/settings' component={Settings}/>
+                    {/*<Dialogs />*/}
+                    {/*<Profile/>*/}
+                </div>
             </div>
-        </div>
+        </BrowserRouter>
     );
 }
 
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -18,19 +18,19 @@ const Navbar = () => {
     return (
         <nav className={classes.nav}>
             <div className={classes.item}>
-                <a>Profile</a>
+                <a href='/profile'>Profile</a>
             </div>
             <div className={`${classes.item} ${classes.active}`}>
-                <a>Massages</a>
+                <a href='/dialogs'>Massages</a>
             </div>
             <div className={classes.item}>
-                <a>News</a>
+                <a href='/news'>News</a>
             </div>
             <div className={classes.item}>
-                <a>Music</a>
+                <a href='/music'>Music</a>
             </div>
             <div className={classes.item}>
-                <a>Settings</a>
+                <a href='/settings'>Settings</a>
             </div>
         </nav>
     );
