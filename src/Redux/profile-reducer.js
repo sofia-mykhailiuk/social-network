25959b2bdba726eecb24620153deb3f383cee066
Sofia Mykhailiuk <sofijka.ukraine@gmail.com>
2021-09-06 19:08:09 +0300

    47. add stateCopy

--- src/Redux/profile-reducer.js
+++ src/Redux/profile-reducer.js
@@ -1,40 +1,49 @@
-const ADD_POST = 'ADD-POST';
-const UPDATE_NEW_POST_VALUE = 'UPDATE-NEW-POST-VALUE';
+const ADD_POST = "ADD-POST";
+const UPDATE_NEW_POST_VALUE = "UPDATE-NEW-POST-VALUE";
 
 let initialState = {
-    postValue: '',
-    posts: [
-        {id: '1', message: 'Hi, how are you?', likesCount: 4},
-        {id: '2', message: 'This is my first post', likesCount: 199},
-        {id: '3', message: 'What`s up', likesCount: 13},
-        {id: '4', message: 'Yo', likesCount: 1}
-    ]
-}
-
-const profileReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case UPDATE_NEW_POST_VALUE:
-            state.postValue = action.text;
-            return state;
-        case ADD_POST:
-            let lastId = state.posts.length;
-            let newPost = {
-                id: (++lastId).toString(),
-                message: state.postValue,
-                likesCount: 0
-            };
-            state.posts.push(newPost);
-            state.postValue = '';
-            return state;
-        default:
-            return state;
-    }
-
+  postValue: "",
+  posts: [
+    { id: "1", message: "Hi, how are you?", likesCount: 4 },
+    { id: "2", message: "This is my first post", likesCount: 199 },
+    { id: "3", message: "What`s up", likesCount: 13 },
+    { id: "4", message: "Yo", likesCount: 1 },
+  ],
 };
 
-export const updateNewPostValueActionCreator = (newValue) =>
-    ({type: UPDATE_NEW_POST_VALUE, text: newValue});
+const profileReducer = (state = initialState, action) => {
 
-export const addPostActionCreator = () => ({type: ADD_POST});
+  switch (action.type) {
+    case UPDATE_NEW_POST_VALUE: {
+      let stateCopy = {...state};
 
-export default profileReducer
+      stateCopy.postValue = action.text;
+      return stateCopy;
+    };
+    case ADD_POST: {
+      let stateCopy = {...state};
+      stateCopy.posts = [...state.posts];
+
+      let lastId = stateCopy.posts.length;
+      let newPost = {
+        id: (++lastId).toString(),
+        message: stateCopy.postValue,
+        likesCount: 0,
+      };
+      stateCopy.posts.push(newPost);
+      stateCopy.postValue = "";
+      return stateCopy;
+    };
+    default:
+      return state;
+  }
+};
+
+export const updateNewPostValueActionCreator = (newValue) => ({
+  type: UPDATE_NEW_POST_VALUE,
+  text: newValue,
+});
+
+export const addPostActionCreator = () => ({ type: ADD_POST });
+
+export default profileReducer;
--- src/index.js
+++ src/index.js
@@ -5,26 +5,15 @@ import App from "./App";
 import "./index.css";
 import store from "./Redux/redux-store";
 import reportWebVitals from "./reportWebVitals";
-import {Provider} from "react-redux"
+import { Provider } from "react-redux";
 
-let rerenderEntireTree = (state) => {
-  ReactDOM.render(
-    <React.StrictMode>
-      <BrowserRouter>
-        <Provider store={store}>
-          <App />
-        </Provider>
-      </BrowserRouter>
-    </React.StrictMode>,
-    document.getElementById("root")
-  );
-};
-
-rerenderEntireTree(store.getState());
-
-store.subscribe(() => {
-  let state = store.getState();
-  rerenderEntireTree(state);
-});
-
-reportWebVitals();
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
