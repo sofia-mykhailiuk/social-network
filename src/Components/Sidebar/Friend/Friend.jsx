bd592e85f431e01e6c633452176ae13c3234462a
Sofia Mykhailiuk <sofijka.ukraine@gmail.com>
2021-10-18 11:32:11 +0300

    53. extends React.Component

--- src/Components/Sidebar/Friend/Friend.jsx
+++ src/Components/Sidebar/Friend/Friend.jsx
@@ -1,10 +1,11 @@
 import classes from './Friend.module.css';
+import userPhoto from "../../../assets/images/no-image-user-1536x1536.jpeg";
 
 const Friend = (props) => {
     return (
         <div className={classes.friend}>
             <div className={`avatar`}>
-                <img src={props.state.img} alt=""/>
+                <img src={ props.state.photos.large != null ? props.state.photos.large : userPhoto } alt=""/>
             </div>
             <div>
                 {props.state.name}
--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,13 +1,27 @@
 import classes from './Sidebar.module.css';
 import Friend from "./Friend/Friend";
+import React from "react";
+import * as axios from "axios";
 
-const Sidebar = (props) => {
-    let sidebarElements = props.state.friends.map((d) => <Friend state={d} key={d.id}/>);
-    return (
-        <div className={classes.sidebar}>
-            {sidebarElements}
-        </div>
-    )
+class Sidebar extends React.Component {
+    constructor (props) {
+        super(props);
+
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            this.props.setFriends(response.data.items);
+        });
+    }
+
+    render () {
+        return (
+            <div className={classes.sidebar}>
+                {this.props.state.friends.map((item, index) => {
+                    return index<6 ? <Friend state={item} key={item.id}/> : null
+                })}
+            </div>
+        )
+    }
 }
+
 
 export default Sidebar
